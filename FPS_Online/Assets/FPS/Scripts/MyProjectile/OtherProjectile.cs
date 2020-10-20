5680cae5d71d95fa8420210656b5b606b072cc35
theixgamer <theofritaku@gmail.com>
2020-10-20 16:07:14 -0300

    movimiento de bala

---
+++ FPS_Online/Assets/FPS/Scripts/MyProjectile/OtherProjectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherProjectile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    public void Movement()
+    {
+        transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Damageable damageable = other.GetComponent<Damageable>();
+        if (damageable != null)
+        {
+            damageable.InflictDamage(damage, false, gameObject);
+        }
+    }
+}
